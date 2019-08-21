2fad34263d0d7f868417f1cd7602ef83ea8feb2c
Eren Livingstone <eren.livingstone@gmail.com>
2019-08-20 23:55:15 -0400

    Implement skeleton of collection view data source, placeholder delegate method for sizing cells, and property to store fetched API results

--- Swift Digest/Swift Digest/Listings/ListingsViewController.swift
+++ Swift Digest/Swift Digest/Listings/ListingsViewController.swift
@@ -14,10 +14,15 @@ class ListingsViewController: UIViewController {
 
 	@IBOutlet private var listingsCollectionView: UICollectionView!
 	
+	private var listings = [Listing]()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		listingsCollectionView.delegate = self
+		listingsCollectionView.dataSource = self
+		
+		listingsCollectionView.register(UINib(nibName: "\(ListingCollectionViewCell.self)", bundle: nil), forCellWithReuseIdentifier: ListingCollectionViewCell.reuseIdentifier)
 		
 		fetchSwiftSubreddit()
 	}
@@ -56,7 +61,13 @@ class ListingsViewController: UIViewController {
 				for listing in listings {
 					print("\(listing.data.title) -- \(listing.data.author) -- \(listing.data.selftext)\n")
 				}
-				// TODO: send results to the datasource and reload the collectionView
+				
+				// Clear previously cached data
+				// FIXME: probably want to convert this to an append that has a .contains() check
+				self.listings = []
+				// Cache parsed results and reload the collectionView
+				self.listings = listings
+				self.reloadData()
 			} catch let decodeError {
 				// TODO: handle error
 				print(decodeError)
@@ -73,5 +84,24 @@ class ListingsViewController: UIViewController {
 }
 
 extension ListingsViewController: UICollectionViewDelegate {
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		// TODO: implement this to handle cells that have images (maintaining image aspect ratio) and titles, or those that just have titles
+		return CGSize.zero
+	}
+}
+
+extension ListingsViewController: UICollectionViewDataSource {
+	func numberOfSections(in collectionView: UICollectionView) -> Int {
+		return 1
+	}
 	
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return listings.count
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ListingCollectionViewCell.reuseIdentifier, for: indexPath)
+		// TODO: set cell's content
+		return cell
+	}
 }
