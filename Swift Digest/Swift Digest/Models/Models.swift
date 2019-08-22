71e2cf06f8957f4596f93ec234b01a6e9d2819d1
Eren Livingstone <eren.livingstone@gmail.com>
2019-08-21 21:27:19 -0400

    Model changes to support parsing listing thumbnail and size

--- Swift Digest/Swift Digest/Models/Models.swift
+++ Swift Digest/Swift Digest/Models/Models.swift
@@ -25,19 +25,27 @@ struct Listing: Codable {
 }
 
 struct ListingData: Codable {
-	var author: String
+//	var author: String
 //	var created: Double
 	var createdUTC: Double
-	var numComments: Int
-	var selftext: String
+	var id: String
+//	var numComments: Int
+//	var selftext: String
 	var title: String
+	var thumbnail: String?
+	var thumbnailHeight: Double?
+	var thumbnailWidth: Double?
 	
 	enum CodingKeys: String, CodingKey {
-		case author
+//		case author
 //		case created
 		case createdUTC = "created_utc"
-		case numComments = "num_comments"
-		case selftext
+		case id
+//		case numComments = "num_comments"
+//		case selftext
 		case title
+		case thumbnail
+		case thumbnailHeight = "thumbnail_height"
+		case thumbnailWidth = "thumbnail_width"
 	}
 }
